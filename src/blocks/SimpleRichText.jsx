b84387a01869cc2e97de6e346c3addfe116bb491
Benjamin Saks <saksben2@gmail.com>
2024-04-08 01:16:26 -0700

    add dynamic blog posts - advanced method - NOTE there is a process bug with how it retrieves the body data or how it interacts with the rich text editor, only retrieves one for all

--- src/blocks/SimpleRichText.jsx
+++ src/blocks/SimpleRichText.jsx
@@ -1,10 +1,10 @@
-import RichTextParser from '@/utils/RichTextParser'
-import React from 'react'
+import RichTextParser from "@/utils/RichTextParser";
+import React from "react";
 
-export default function SimpleRichText({body}) {
+export default function SimpleRichText({ body }) {
   return (
-    <div className='py-10 px-6'>
-        <RichTextParser content={body} />
+    <div className="py-10 px-6">
+      <RichTextParser content={body} />
     </div>
-  )
+  );
 }
--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -11,34 +11,86 @@ export default function Page({ page }) {
   );
 }
 
-// For every page, it will return the slug
 export const getStaticPaths = async () => {
-  const pageReq = await axios(`/api/pages?limit=100`);
-  const pageData = pageReq.data;
+    const pageReq = await axios(`/api/pages?limit=100`);
+    const pageData = pageReq.data;
 
-  const returnObj = {
-    paths: pageData.docs.map(({ slug, id }) => {
-      return {
-        params: { slug: slug !== "index" ? slug.split("/") : false },
-      };
-    }),
-    fallback: false,
-  };
+    const pagesToSave = pageData.docs
 
-  return returnObj;
+
+    const templateReq = await axios(`/api/pageTemplates?limit=100`);
+    const pageTemplates = templateReq.data.docs;
+
+    for (const template of pageTemplates) {
+        // fetch data from template collection
+        const templateCollectionReq = await axios(`/api/${template.templateCollection}?limit=100`);
+        const templateCollectionData = templateCollectionReq.data.docs;
+
+
+        for (const item of templateCollectionData) {
+
+            const populatedLayout = template.layout.map((layoutBlock) => {
+                if (layoutBlock.blockType) { }
+
+                // Replace placeholders (<%= ... %>) in layout
+                let layoutString = JSON.stringify(layoutBlock);
+                // Parse EJS Placeholders
+                const regex = /<%=(.*?)%>/g;
+                const matches = layoutString.match(regex);
+                if (matches) {
+                    for (const match of matches) {
+                        const key = match.replace('<%=', '').replace('%>', '').trim();
+                        const value = item[key];
+
+                        // If it's a rich Text, replace the entire field
+                        if (value?.root?.children?.length) {
+                            layoutBlock[key] = value;
+                            return layoutBlock;
+                        } else {
+                            // Else just replace the specific parts
+                            layoutString = layoutString.replace(match, value);
+                        }
+                    }
+                }
+                return (JSON.parse(layoutString));
+            })
+
+            pagesToSave.push({
+                ...item,
+                slug: `${template.slug}/${item.slug}`,
+                layout: populatedLayout
+            })
+        }
+    }
+
+    const fs = await import('fs');
+    fs.writeFileSync('./src/pages.json', JSON.stringify(pagesToSave, null, 2));
+
+
+    const returnObj = {
+        paths: pagesToSave.map((page) => ({
+            params: {
+                slug: page.slug === 'index' ? [] : page.slug.split('/'),
+            },
+        })),
+        fallback: false,
+    };
+
+    return returnObj;
 };
 
-// Fetches all page-specific data and renders it
 export const getStaticProps = async (ctx) => {
-  const slug = ctx.params?.slug || "index";
+    const slug = ctx.params?.slug || ['index'];
 
-  // Fetch page
-  const pageReq = await axios(`/api/pages?where[slug][equals]=${slug}`);
-  let pageData = pageReq.data.docs[0];
+    const fs = await import('fs');
+    const pages = JSON.parse(fs.readFileSync('./src/pages.json', 'utf8'));
+    const pageData = pages.find((page) => page.slug === slug.join('/'));
 
-  return {
-    props: {
-      page: pageData,
-    },
-  };
+
+    return {
+        props: {
+            page: pageData,
+        },
+    };
+
 };
